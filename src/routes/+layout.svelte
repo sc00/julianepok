2986ceb886b1cb472fe29b4871dad5f842f3e690
sc00 <mail@julian-urabl.com>
2026-04-13 19:05:43 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	/**
+	 * IMPORTS
+	 */
+	import '$lib/scss/common.scss';
+
+	/**
+	 * PROPS
+	 */
+	const { children } = $props();
+
+	/**
+	 * FUNCTIONS
+	 */
+</script>
+
+{@render children?.()}
