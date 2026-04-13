31b35f09eaed9ab7e83b2e4863cae6c0b131ef80
sc00 <mail@julian-urabl.com>
2026-04-13 19:11:07 +0200

    add portfolio data

---
+++ src/lib/constants/portfolio.ts
@@ -0,0 +1,198 @@
+export const portfolio = [
+	{
+		year: '2026',
+		title: 'Between Two Sells1',
+		type: 'Monograph',
+		medium: 'Arch File Folder',
+		location: 'TAS (Publication)'
+	},
+	{
+		year: '2026',
+		title: 'Une Piscine1',
+		type: 'Installation',
+		medium: 'Neon Light Tubes / Aluminum',
+		location: 'Louvre (Paris)'
+	},
+	{
+		year: '2026',
+		title: 'The Fresh Taste of Apple in My Mouth1',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'TAS (Nicosia)'
+	},
+	{
+		year: '2026',
+		title: 'The Rest Is Silence1',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'TAS (Athens)'
+	},
+	{
+		year: '2026',
+		title: 'Please God Save Us All1',
+		type: 'Series',
+		medium: 'Gas Discharge Tubes / Aluminum',
+		location: 'TAS (Vienna)'
+	},
+	{
+		year: '2026',
+		title: 'Aphrodisia',
+		type: 'Solo Exhibition',
+		medium: 'Digital Print / PVC Canvas',
+		location: 'Private Collection (Nicosia)'
+	},
+	{
+		year: '2025',
+		title: 'Meer als alles andere',
+		type: 'Series',
+		medium: 'Gas Discharge Tubes / Aluminum',
+		location: 'TAS (Vienna)'
+	},
+	{
+		year: '2025',
+		title: 'Balance of Force',
+		type: 'Solo Exhibition',
+		medium: 'Neon Light, Digital Print / Canvas',
+		location: 'Private Collection (Bregenz)'
+	},
+	{
+		year: '2024',
+		title: 'Hillside Headstone',
+		type: 'Installation',
+		medium: 'Mixed Media',
+		location: 'TAS (Vienna)'
+	},
+	{
+		year: '2024',
+		title: 'Corpus One',
+		type: 'Solo Exhibition',
+		medium: 'Gas Discharge Tube, Hydro Motors',
+		location: 'Loos Haus (Vienna)'
+	},
+	{
+		year: '2024',
+		title: 'Heaven Holds the Faithfull Departed',
+		type: 'Solo Exhibition',
+		medium: 'Neon Light, Acrylic / Canvas',
+		location: 'Art Week (Berlin)'
+	},
+	{
+		year: '2024',
+		title: 'Forver Paintings',
+		type: 'Series',
+		medium: 'Gas Discharge Tube, Acrylic / Canvas',
+		location: 'Private Collection (Norway)'
+	},
+	{
+		year: '2024',
+		title: 'Where Can I Meet You',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'TAS (Vienna)'
+	},
+	{
+		year: '2023',
+		title: 'Cash, Cum and Thunderstorms Pt II',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'TAS (Vienna)'
+	},
+	{
+		year: '2023',
+		title: 'Cash, Cum and Thunderstorms Pt I',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'TAS (Lefkara)'
+	},
+	{
+		year: '2023',
+		title: 'Soulful Hardcore',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'TAS (Lefkara)'
+	},
+	{
+		year: '2023',
+		title: 'Affirmation for Angelina',
+		type: 'Installatin',
+		medium: 'Gas Discharge Tube / Alu Frames',
+		location: 'Private Collection (Limassol)'
+	},
+	{
+		year: '2022',
+		title: 'Chamapgne Supernova',
+		type: 'Solo Exhibition',
+		medium: 'Acrylic / PVC Canvas',
+		location: 'Atelier (Vienna)'
+	},
+	{
+		year: '2022',
+		title: 'The Spirit of Exctasy',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'Atelier (Vienna)'
+	},
+	{
+		year: '2022',
+		title: 'As a Hole',
+		type: 'Solo Exhibition',
+		medium: 'Neon Light, Acrylic / Canvas',
+		location: 'Atelier (Vienna)'
+	},
+	{
+		year: '2021',
+		title: 'Everything I Love, For Ever',
+		type: 'Series',
+		medium: 'Mixed Media / Canvas',
+		location: 'Private Collection (Velden)'
+	},
+	{
+		year: '2020',
+		title: 'The Refelction of Our Knowledge',
+		type: 'Installation',
+		medium: 'Neon Light, Digital Print / Canvas',
+		location: 'Private Collection (Cologne)'
+	},
+	{
+		year: '2019',
+		title: 'Animus',
+		type: 'Solo Exhibition',
+		medium: 'Sugar, Acrylic, Dig Print / PVC Canvas',
+		location: 'FJK Suite (Vienna)'
+	},
+	{
+		year: '2019',
+		title: 'Wet Storage Oyster Culter',
+		type: 'Solo Exhibition',
+		medium: 'Mixed Media',
+		location: 'Reaktor (Vienna)'
+	},
+	{
+		year: '2017',
+		title: 'Silent',
+		type: 'Solo Exhibition',
+		medium: 'Acrylic, Digital Print / Canvas',
+		location: 'Tile Gallery (Vienna)'
+	},
+	{
+		year: '2015',
+		title: 'Love & Happiness',
+		type: 'Solo Exhibition',
+		medium: 'Acrylic / Canvas',
+		location: 'Galerie Sonnenfels (Vienna)'
+	},
+	{
+		year: '2014',
+		title: 'Love & Doom',
+		type: 'Solo Exhibition',
+		medium: 'Acrylic / Canvas',
+		location: 'F6 (Vienna)'
+	},
+	{
+		year: '2013',
+		title: 'Madness & Beauty',
+		type: 'Solo Exhibition',
+		medium: 'Acrylic / Canvas',
+		location: 'WUK (Vienna)'
+	}
+];
