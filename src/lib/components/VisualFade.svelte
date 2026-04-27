3b42552e25d15d75d3e711ceed2c8e59f1b37ef5
sc00 <mail@julian-urabl.com>
2026-04-27 13:09:28 +0200

    add fade to portfolio item toggle

---
+++ src/lib/components/VisualFade.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	/**
+	 * IMPORTS
+	 */
+	import type { Snippet } from 'svelte';
+	import { fade, type FadeParams } from 'svelte/transition';
+
+	/**
+	 * TYPES
+	 */
+	interface Props {
+		fadeIn?: FadeParams;
+		fadeOut?: FadeParams;
+		children?: Snippet;
+	}
+
+	/**
+	 * PROPS
+	 */
+	let { fadeIn, fadeOut, children }: Props = $props();
+</script>
+
+<div in:fade={fadeIn} out:fade={fadeOut}>
+	{@render children?.()}
+</div>
